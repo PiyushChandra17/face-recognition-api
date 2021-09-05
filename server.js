afc017f488efdb98d7e54e32a0c6e91584e0f6b2
piyush1795 <chandra.piyush17@gmail.com>
2021-09-05 19:12:05 +0530

    updating database

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const knex = require('knex');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-infinite-16825',
       port : 5432,
       user : 'piyush',
       password : '',
